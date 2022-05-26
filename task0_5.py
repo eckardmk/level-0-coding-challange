19e4faa21b99058f2ce3a24d28b0beb64e4a44c5
eckardmk <104773689+eckardmk@users.noreply.github.com>
2022-05-26 16:07:25 +0200

    Update task0_5.py

--- task0_5.py
+++ task0_5.py
@@ -1,10 +1,7 @@
-import math
-
-
 def Area_of_Triangle(a, b, c):
 
     s = (a + b + c) / 2
-    Area = math.sqrt((s * (s - a) * (s - b) * (s - c)))
+    Area = ((s * (s - a) * (s - b) * (s - c)))**0.5
 
     return Area
 
