1b7ddbadee38c86ff925e5c9ac901b3f058622d3
eckardmk <104773689+eckardmk@users.noreply.github.com>
2022-05-27 10:43:33 +0200

    Update task0_9.py

--- task0_9.py
+++ task0_9.py
@@ -1,9 +1,12 @@
-word = input("Enter word: ")
-for letter in word.lower():
-    if letter in "aeiou":
-        print(letter)
+def vowels(word):
+    for letter in word.lower():
+        if letter in "aeiou":
+            print(letter)
 
-x = list()
-for letter in word.lower():
-    if letter in "aeiou" and letter not in x:
-        x.append(letter)
+    x = list()
+    for letter in word.lower():
+        if letter in "aeiou" and letter not in x:
+            x.append(letter)
+
+
+vowels("Umuzi")
