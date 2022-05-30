1171594ebe914780deab5c1796d0c11179748844
eckardmk <104773689+eckardmk@users.noreply.github.com>
2022-05-30 11:32:07 +0200

    Update task0_4.py

--- task0_4.py
+++ task0_4.py
@@ -1,9 +1,11 @@
 def even_or_odd(num):
 
     if num % 2 == 0:
-        print(num, " Is an even number")
+        print(num,"is an even number")
     else:
-        print(num, " is an odd number")
+        print(num,"is an odd number")
 
 
 even_or_odd(57)
+
+
